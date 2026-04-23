fecf484e0834caba70b1aa79ce6381bb84d5186a
Palawi <bapcoloj@gmail.com>
2026-04-23 22:07:27 +0200

    fix(config): add missing eslint eslintrc package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -5,9 +6,7 @@ import { FlatCompat } from '@eslint/eslintrc'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
